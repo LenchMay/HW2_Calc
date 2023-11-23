ea4f40db4a889be317451b94d960734e34c5655a
Elena Mayorova <lenchikmay@bk.ru>
2023-11-23 21:53:44 +0300

    UI making

--- app/src/main/java/com/example/hw2_calc/MainActivity.kt
+++ app/src/main/java/com/example/hw2_calc/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.hw2_calc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hw2_calc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.btnPlus.setOnClickListener{
+
+        }
     }
 }
