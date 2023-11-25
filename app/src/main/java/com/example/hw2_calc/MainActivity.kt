76ab06cca7e7d663e1761e4a420f205e2ba63fdb
Elena Mayorova <lenchikmay@bk.ru>
2023-11-25 16:40:25 +0300

    calculator logic

--- app/src/main/java/com/example/hw2_calc/MainActivity.kt
+++ app/src/main/java/com/example/hw2_calc/MainActivity.kt
@@ -8,18 +8,61 @@ import com.example.hw2_calc.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var operand1: Double = 0.0
+    private var operand2: Double = 0.0
+    private var operator: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         with(binding) {
+
+            btnPlus.setOnClickListener{
+                operatorBtnClick(getString(R.string.plus))
+            }
+            btnMinus.setOnClickListener{
+                operatorBtnClick(getString(R.string.minus))
+            }
+            btnMulti.setOnClickListener{
+                operatorBtnClick(getString(R.string.multiplication))
+            }
+            btnDiv.setOnClickListener{
+                operatorBtnClick(getString(R.string.division))
+            }
             btnAbout.setOnClickListener{
                 val intent = Intent(this@MainActivity, AboutActivity::class.java)
                 startActivity(intent)
             }
         }
+    }
+
+    private fun operatorBtnClick(clickedOperator: String) {
+        operator = clickedOperator
+        with(binding){
+            val operand1Str = operandFirst.text.toString()
+            val operand2Str = operandSecond.text.toString()
+
+            if(operand1Str.isNotEmpty() && operand2Str.isNotEmpty()) {
+                operand1 = operand1Str.toDouble()
+                operand2 = operand2Str.toDouble()
+                val result = when (operator) {
+                    getString(R.string.plus) -> operand1 + operand2
+                    getString(R.string.minus) -> operand1 - operand2
+                    getString(R.string.multiplication) -> operand1 * operand2
+                    getString(R.string.division) -> if (operand2 != 0.0) operand1 / operand2 else Double.NaN
+                    else -> Double.NaN
+                }
+                val resultFormat = getString(R.string.result_format)
+                val resultStr = String.format(resultFormat, operand1, operator, operand2, result)
+                resultText.text = resultStr
+            } else {
+                resultText.text = ""
+            }
+        }
 
     }
+
 
 }
